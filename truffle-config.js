4bbd99fcb78060d6e94280f462c61a32d8715572
unknown <mohamed.elbaruni@gmail.com>
2019-11-08 22:31:46 +0200

    updates

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,6 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() {
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "Your Metamask seeds or any wallet seeds";
+var NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
 
 module.exports = {
   networks: {
@@ -19,6 +9,21 @@ module.exports = {
       port: "7545",
       gas: 4600000,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: function() {
+        var wallet = new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/v3/2a3ef5517fce4991b5d2c4cf004ff40d" //endpoint to ethereum blockchain
+        );
+        var nonceTracker = new NonceTrackerSubprovider();
+        wallet.engine._providers.unshift(nonceTracker);
+        nonceTracker.setEngine(wallet.engine);
+        return wallet;
+      },
+      network_id: 4
+      // gas: 2000000,   // <--- Twice as much
+      // gasPrice: 10000000000,
     }
   }
   // See <http://truffleframework.com/docs/advanced/configuration>
